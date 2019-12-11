24564367a847da7dcc3eb8cca97abafd8c738ae9
sarquamon <sarquamon@gmail.com>
2019-12-10 21:59:29 -0600

    Added support for mongo with mongoose, products can be updated, deleted, inserted and shown

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,34 +1,135 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("../../models/products");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-	res.status(200).json({
-		message: "You're in GET method on /products"
-	});
+	Product.find()
+		.exec()
+		.then(result => {
+			if (result) {
+				if (result.length >= 0) {
+					console.log(`Success! ${result}`);
+					res.status(200).json(result);
+				} else {
+					console.log(`No entries! ${result}`);
+					res
+						.status(404)
+						.json({message: `No entries have been found ${result}`});
+				}
+			} else {
+				console.log(`Not found! ${result}`);
+				res.status(404).json({message: `Not found! ${result}`});
+			}
+		})
+		.catch(err => {
+			console.log(`Error! ${err}`);
+			res.status(500).json({error: err});
+		});
 });
 
 router.post("/", (req, res, next) => {
 	const {productName, productPrice} = req.body;
-	res.status(201).json({
-		message: "You're in POST method on /products",
-		productName: `This is the product name: ${productName}`,
-		productPrice: `This is the product price: ${productPrice}`
+
+	const product = new Product({
+		_id: new mongoose.Types.ObjectId(),
+		productName: productName,
+		productPrice: productPrice
 	});
+
+	product
+		.save()
+		.then(result => {
+			console.log(`Success! ${result}`);
+			res.status(201).json({
+				message: "You're in POST method on /products",
+				createdProduct: result
+			});
+		})
+		.catch(err => {
+			console.log(`Error! ${err}`);
+			res.status(500).json({error: err});
+		});
 });
 
 router.get("/:productId", (req, res, next) => {
 	const {productId} = req.params;
 
-	if (productId === "salomon") {
-		res.status(200).json({
-			message: `hola ${productId}`,
-			id: productId
-		});
+	if (productId) {
+		Product.findById(productId)
+			.exec()
+			.then(result => {
+				if (result) {
+					console.log(`Success! ${result}`);
+					res.status(200).json(result);
+				} else {
+					console.log(`Not found! ${result}`);
+					res.status(404).json({message: "Not found"});
+				}
+			})
+			.catch(err => {
+				console.log(`Error! ${err}`);
+				res.status(500).json({error: err});
+			});
 	} else {
-		res.status(200).json({
-			message: `Hola man`
-		});
+		console.log(`Not found! ${productId}`);
+		res.status(404).json({message: `Not found! ${productId}`});
+	}
+});
+
+router.delete("/:productId", (req, res, next) => {
+	const {productId} = req.params;
+
+	if (productId) {
+		Product.remove({_id: productId})
+			.exec()
+			.then(result => {
+				if (result) {
+					console.log(`Success! ${result}`);
+					res.status(200).json(result);
+				} else {
+					console.log(`Not found! ${productId}`);
+					res.status(404).json({message: `Not found! ${productId}`});
+				}
+			})
+			.catch(err => {
+				console.log(`Error! ${err}`);
+				res.status(500).json({error: err});
+			});
+	} else {
+		console.log(`Not found! ${productId}`);
+		res.status(404).json({message: `Not found! ${productId}`});
+	}
+});
+
+router.patch("/:productId", (req, res, next) => {
+	const {productId} = req.params;
+	if (productId) {
+		const updateOps = {};
+
+		for (const ops of req.body) {
+			updateOps[ops.propName] = ops.value;
+		}
+
+		Product.update({_id: productId}, {$set: updateOps})
+			.exec()
+			.then(result => {
+				if (result) {
+					console.log(`Success! ${result}`);
+					res.status(200).json(result);
+				} else {
+					console.log(`Not found! ${productId}`);
+					res.status(404).json({message: `Not found! ${productId}`});
+				}
+			})
+			.catch(err => {
+				console.log(`Error! ${err}`);
+				res.status(500).json({error: err});
+			});
+	} else {
+		console.log(`Not found! ${productId}`);
+		res.status(404).json({message: `Not found! ${productId}`});
 	}
 });
 
--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+const MONGODBURL = `mongodb+srv://admin:${process.env.MONGODB_ATLAS_PWD}@developmentapps-lmcx2.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(
+	MONGODBURL,
+	{useNewUrlParser: true, useUnifiedTopology: true},
+	err => {
+		if (!err) {
+			console.log("Succesful MongoDB connection");
+		} else {
+			console.log(`Connection error ${err}`);
+		}
+	}
+);
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
